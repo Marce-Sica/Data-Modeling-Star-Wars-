22bb2e044451ab291b413a5b480304e88706ba60
Marcelo Sica <marcelosica10@gmail.com>
2023-04-26 04:10:17 +0000

    Proyecto Data Modeling Star Wars

--- src/models.py
+++ src/models.py
@@ -9,56 +9,56 @@ Base = declarative_base()
 
 class Characters(Base):
     __tablename__ = 'Characters'
-    ID = Column(Integer, primary_key=True)
-    Name = Column(String(50), nullable=False)
-    BirthYear = Column(String(50), nullable=False)
-    Gender = Column(String(50), nullable=False)
-    EyesColor = Column(String(50), nullable=False)
-    Skin = Column(String(50), nullable=False)
-    Height = Column(String(50), nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    birth_year = Column(String(50), nullable=False)
+    gender = Column(String(50), nullable=False)
+    eyes_color = Column(String(50), nullable=False)
+    skin = Column(String(50), nullable=False)
+    height = Column(String(50), nullable=False)
     pass
 
 class Planets(Base):
     __tablename__ = 'Planets'
-    ID = Column(Integer, primary_key=True)
-    Name = Column(String(50), nullable=False)
-    Gravity = Column(String(50), nullable=False)
-    Terrain = Column(String(50), nullable=False)
-    Climate = Column(String(50), nullable=False)
-    OrbitalPeriod = Column(String(50), nullable=False)
-    Population = Column(String(50), nullable=False)
-    Diameter = Column(String(50), nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    gravity = Column(String(50), nullable=False)
+    terrain = Column(String(50), nullable=False)
+    climate = Column(String(50), nullable=False)
+    orbital_period = Column(String(50), nullable=False)
+    population = Column(String(50), nullable=False)
+    diameter = Column(String(50), nullable=False)
     pass
 
 class Vehicles(Base):
     __tablename__ = 'Vehicles'
-    ID = Column(Integer, primary_key=True)
-    Model = Column(String(50), nullable=False)
-    Length = Column(String(50), nullable=False)
-    MaxSpeed = Column(String(50), nullable=False)
-    CargoCapacity = Column(String(50), nullable=False)
-    Class = Column(String(50), nullable=False)
-    Manufacture = Column(String(100), nullable=False)
+    id = Column(Integer, primary_key=True)
+    model = Column(String(50), nullable=False)
+    length = Column(String(50), nullable=False)
+    max_speed = Column(String(50), nullable=False)
+    cargo_capacity = Column(String(50), nullable=False)
+    class_ = Column(String(50), nullable=False)
+    manufacture = Column(String(100), nullable=False)
     pass
 
 class Favorites(Base):
     __tablename__ = 'Favorites'
-    ID = Column(Integer, primary_key=True)
-    Characters_id = Column(Integer, ForeignKey('Characters.id'))
-    Planets_id = Column(Integer, ForeignKey('Planets.id'))
-    Vehicles_id = Column(Integer, ForeignKey('Vehicles.id'))
+    id = Column(Integer, primary_key=True)
+    characters_id = Column(Integer, ForeignKey('Characters.id'))
+    planets_id = Column(Integer, ForeignKey('Planets.id'))
+    vehicles_id = Column(Integer, ForeignKey('Vehicles.id'))
     pass
 
 
 class User(Base):
     __tablename__ = 'User'
-    UserID = Column(Integer, primary_key=True)
-    FirstName = Column(String(50), nullable=False)
-    LastName = Column(String(50), nullable=False)
-    UserName = Column(String(50), nullable=False)
-    Email = Column(String(50), nullable=False)
-    Password= Column(Integer, primary_key=True)
-    Favorites_id = Column(Integer, ForeignKey('Favorites.id'))
+    user_id = Column(Integer, primary_key=True)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    user_name = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False)
+    password= Column(Integer, primary_key=True)
+    favorites_id = Column(Integer, ForeignKey('Favorites.id'))
     pass
 
 ## Draw from SQLAlchemy base
